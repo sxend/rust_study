bcdfc85590e1a9c6f95e904faa7aac094494963e
sxend <arimitsu.k+github@gmail.com>
2017-07-09 15:25:38 +0900

    add server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate hyper;
+extern crate futures;
+
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
+
+struct HelloWorld;
+
+const PHRASE: &str = "Hello, World!";
+
+impl Service for HelloWorld {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+
+    fn call(&self, _req: Request) -> Self::Future {
+        futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(PHRASE.len() as u64))
+                .with_body(PHRASE)
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+    server.run().unwrap();
 }
