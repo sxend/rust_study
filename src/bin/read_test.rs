3ef31754a11471c3a9b82e98ff850f5da0ed3432
sxend <arimitsu.k+github@gmail.com>
2017-08-01 00:40:02 +0900

    add string reader

---
+++ src/bin/read_test.rs
@@ -0,0 +1,35 @@
+
+use std::io::{Cursor, BufRead};
+
+fn main() {
+    let target_str = "\
+        include \"./foo.conf\"
+        foo {
+            bar.bazz {
+                value = \"ああああ\"
+
+                arr = []
+            }
+        }
+
+
+
+aaa = \"aaa\"
+
+    ".to_string();
+    let mut lines = Cursor::new(target_str).lines();
+    while let Some(Ok(line)) = lines.next() {
+        match line {
+            _ if line.starts_with(INCLUDE) => {
+                let line = line.replace(INCLUDE, "");
+                let line = line.trim();
+                println!("include: {}", &line[1..line.len() - 1]);
+            },
+            _ => {
+                println!("normal line: {}", line);
+            }
+        }
+    }
+}
+
+const INCLUDE: &'static str = "include";
--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,7 @@ fn gen_data() -> ResponseData<Message> {
 }
 
 fn main() {
+    println!("run main");
     let addr = "127.0.0.1:3000".parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(Server)).unwrap();
     server.run().unwrap();
