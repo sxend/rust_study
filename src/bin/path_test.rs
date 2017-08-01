e2aae0a70f077ac62e8a8d374ca862523ab93f5e
sxend <arimitsu.k+github@gmail.com>
2017-08-01 23:19:33 +0900

    add file resolve test

---
+++ src/bin/path_test.rs
@@ -0,0 +1,16 @@
+use std::path::{Path, PathBuf};
+
+fn main() {
+    let include_line_path: PathBuf = Path::new("../main.rs").to_owned();
+    let include_line_path: PathBuf = if include_line_path.is_absolute() {
+        include_line_path
+    } else if include_line_path.is_relative() {
+        let current_path = Path::new("./src/bin/path_test.rs");
+        let mut path_buf = current_path.parent().unwrap().to_path_buf();
+        path_buf.push(&include_line_path);
+        path_buf
+    } else {
+        panic!("unexpected path: {:?}", include_line_path);
+    };
+    println!("include_line_file: {:?}, exists: {}", include_line_path, include_line_path.exists());
+}
