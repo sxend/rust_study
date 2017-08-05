7256f7de5494da69e38fcaa6baae6352e92705ee
sxend <arimitsu.k+github@gmail.com>
2017-08-05 21:19:02 +0900

    rename samples

--- src/bin/path_sample.rs
+++ src/bin/path_sample.rs
@@ -1,11 +1,11 @@
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let include_line_path: PathBuf = Path::new("../main.rs").to_owned();
+    let include_line_path: PathBuf = Path::new("./src/bin/path_sample.rs").to_owned();
     let include_line_path: PathBuf = if include_line_path.is_absolute() {
         include_line_path
     } else if include_line_path.is_relative() {
-        let current_path = Path::new("./src/bin/path_test.rs");
+        let current_path = Path::new("./src/bin/path_sample.rs");
         let mut path_buf = current_path.parent().unwrap().to_path_buf();
         path_buf.push(&include_line_path);
         path_buf
