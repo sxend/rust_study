348707d8685a69177a5e199d358eac9930c0afb8
sxend <arimitsu.k+github@gmail.com>
2017-08-06 12:05:45 +0900

    add duration example

--- src/bin/timestamp_sample.rs
+++ src/bin/timestamp_sample.rs
@@ -1,5 +1,8 @@
 extern crate time;
 
+use std::ops::Add;
+
 fn main() {
     println!("{:?}", time::now().to_timespec());
+    println!("{}", time::now().add(time::Duration::seconds(3600)).to_utc().rfc822());
 }
