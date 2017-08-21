c857a104ee8bed18ade515cd132b5127fa211b4d
sxend <arimitsu.k+github@gmail.com>
2017-08-21 23:44:12 +0900

    add pooled thread sample

---
+++ src/bin/thread_pool_sample.rs
@@ -0,0 +1,25 @@
+
+extern crate futures;
+extern crate futures_cpupool;
+
+use futures::future::Future;
+use futures_cpupool::Builder;
+use std::thread;
+
+fn main() {
+    let pool = Builder::new().pool_size(2).name_prefix("prefixed_thread_pool").create();
+    let f0 = pool.spawn_fn(|| { task("task 0") });
+    let f1 = pool.spawn_fn(|| { task("task 1") });
+    let f2 = pool.spawn_fn(|| { task("task 2") });
+
+    let result = f0.join(f1).join(f2).wait();
+    println!("futures result: [{:?}]. current thread: {}", result, current_thread_name());
+}
+
+fn task(name: &str) -> Result<String, String> {
+    Ok::<String, String>(format!("task: [{}]. finish in thread: {}", name, current_thread_name()))
+}
+
+fn current_thread_name() -> String {
+    thread::current().name().unwrap_or("unknown").to_string()
+}
