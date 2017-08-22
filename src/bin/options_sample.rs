d43257606df1554bc37892954967cfe041e05135
sxend <arimitsu.k+github@gmail.com>
2017-08-22 11:38:07 +0900

    add docopt sample

---
+++ src/bin/options_sample.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+options sample command
+
+Usage:
+  options_sample [--speed=<kn>]
+  options_sample (-h | --help)
+  options_sample --version
+
+
+Options:
+  --speed=<kn>  Speed in knots [default: 10].
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_speed: i32,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+}
