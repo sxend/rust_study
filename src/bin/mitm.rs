6b00e8ffd0e3c35e79e7639c4883fc9905aad622
sxend <arimitsu.k+github@gmail.com>
2017-08-22 13:38:51 +0900

    add home dir option

--- src/bin/mitm.rs
+++ src/bin/mitm.rs
@@ -5,7 +5,16 @@ extern crate docopt;
 
 use docopt::Docopt;
 
-const USAGE: &'static str = "
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_port: i32,
+    flag_webui_port: i32,
+    flag_config_dir: String,
+}
+
+fn main() {
+    let home_dir = std::env::var("HOME").unwrap_or("/tmp".to_string());
+    let usage = format!("
 mitm
 
 Usage:
@@ -13,22 +22,14 @@ Usage:
   options_sample (-h | --help)
   options_sample --version
 
-
 Options:
   --port=<p>         bind proxy port [default: 8889].
   --webui-port=<mp>  bind webui port [default: 8899].
+  --config-dir=<dr>  config save dir [default: {HOME}/.mitm].
   -h --help          Show this screen.
   --version          Show version.
-";
-
-#[derive(Debug, Deserialize)]
-struct Args {
-    flag_port: i32,
-    flag_webui_port: i32,
-}
-
-fn main() {
-    let args: Args = Docopt::new(USAGE)
+", HOME = home_dir);
+    let args: Args = Docopt::new(usage)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
