65260a77de9bc69e35c8569eec5bf6479b66b35d
sxend <arimitsu.k+github@gmail.com>
2017-08-24 21:57:38 +0900

    add nested data structure

--- src/bin/nano_template.rs
+++ src/bin/nano_template.rs
@@ -21,7 +21,7 @@ pub fn nano(template: String, nanodata: NanoData) -> String {
 
 type HashRef<T> = Rc<RefCell<HashMap<String, T>>>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct NanoData {
     underlying: HashRef<String>,
     children: HashRef<NanoData>
@@ -35,12 +35,10 @@ impl NanoData {
         }
     }
     pub fn get(&self, key: String) -> String {
-        let key_chain = key.split(".").map(|s| s.to_string()).collect();
-        self.get_by_keys(&key_chain)
+        self.get_by_keys(&NanoData::split_key(key))
     }
     pub fn put(&mut self, key: String, value: String) {
-        let key_chain = key.split(".").map(|s| s.to_string()).collect();
-        self.put_by_keys(&key_chain, value);
+        self.put_by_keys(&NanoData::split_key(key), value);
     }
     fn get_by_keys(&self, keys: &Vec<String>) -> String {
         if keys.len() == 1 {
@@ -58,7 +56,31 @@ impl NanoData {
                 .put_by_keys(&keys.split_first().unwrap().1.to_vec(), value);
         }
     }
-
+    pub fn get_data(&self, key: String) -> NanoData {
+        self.get_data_by_keys(&NanoData::split_key(key))
+    }
+    pub fn put_data(&mut self, key: String, value: NanoData) {
+        self.put_data_by_keys(&NanoData::split_key(key), value);
+    }
+    fn get_data_by_keys(&self, keys: &Vec<String>) -> NanoData {
+        let mut borrowed = (*self.children).borrow().get(keys.index(0)).unwrap().clone();
+        if keys.len() != 1 {
+            borrowed = borrowed.get_data_by_keys(&keys.split_first().unwrap().1.to_vec())
+        }
+        borrowed
+    }
+    fn put_data_by_keys(&mut self, keys: &Vec<String>, value: NanoData) {
+        if keys.len() == 1 {
+            self.children.borrow_mut().insert(keys.index(0).to_owned(), value);
+        } else {
+            self.children.borrow_mut()
+                .entry(keys.index(0).to_owned()).or_insert(NanoData::new())
+                .put_data_by_keys(&keys.split_first().unwrap().1.to_vec(), value);
+        }
+    }
+    fn split_key(key: String) -> Vec<String> {
+        key.split(".").map(|s| s.to_string()).collect()
+    }
 }
 
 #[cfg(test)]
@@ -69,14 +91,22 @@ mod tests {
     hoge
         fuga
     [{string_data}]
+    [{nested}]
     [{nested.data0}]
     [{nested.data1}]
+    [{foo.data.put_data}]
+    [{foo}]
         fizz
     ".to_string();
         let mut data = ::NanoData::new();
         data.put("string_data".to_string(), "string data".to_string());
+        data.put("nested".to_string(), "nested value".to_string());
         data.put("nested.data0".to_string(), "nested data0".to_string());
         data.put("nested.data1".to_string(), "nested data1".to_string());
+        data.put("foo".to_string(), "foo value".to_string());
+        let mut put_data = ::NanoData::new();
+        put_data.put("put_data".to_string(), "put value".to_string());
+        data.put_data("foo.data".to_string(), put_data);
         println!("{}", ::nano(template, data));
     }
 }
