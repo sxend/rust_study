679af246cef6f3b9bd00a3f4a4b06dbeda9783cb
sxend <arimitsu.k+github@gmail.com>
2017-08-29 00:16:46 +0900

    add loop

--- src/bin/mitm_sample.rs
+++ src/bin/mitm_sample.rs
@@ -9,39 +9,38 @@ use tokio_io::{io, AsyncRead};
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
 use futures_cpupool::Builder;
-use std::io::Read;
 use std::io::BufReader;
-use std::error::Error;
+use std::io::Write;
 
 fn main() {
-    let pool = Builder::new().pool_size(2).name_prefix("mitm").create();
+//    let pool = Builder::new().pool_size(2).name_prefix("mitm").create();
     let addr = "0.0.0.0:8888".parse().unwrap();
     let mut core = Core::new().unwrap();
     let handle = core.handle();
     let tcp = TcpListener::bind(&addr, &handle).unwrap();
     let server = tcp.incoming()
-        .for_each(move |(mut tcp, _)| {
-            let (reader, writer) = tcp.split();
+        .for_each(move |(tcp, _)| {
+            let (reader, mut writer) = tcp.split();
             let reader = BufReader::new(reader);
-            let result = io::read_until(reader, b'\n', vec![0u8]).and_then(|(conn, buf)| {
-                println!("{}", std::str::from_utf8(&buf).unwrap());
-                Ok((conn, buf))
+            let mut result = io::read_until(reader, b'\n', vec![0u8]).and_then(|(reader, buf)| {
+                Ok((reader, String::from_utf8(buf.to_vec()).unwrap()))
             }).boxed();
-            let result = result.and_then(|(conn, buf)| {
-                io::read_until(conn, b'\n', vec![0u8]).and_then(|(conn, buf)| {
-                    println!("{}", std::str::from_utf8(&buf).unwrap());
-                    Ok((conn, buf))
-                })
-            }).boxed();
-            let result = result.and_then(|(conn, buf)| {
-                io::read_until(conn, b'\n', vec![0u8]).and_then(|(conn, buf)| {
-                    println!("{}", std::str::from_utf8(&buf).unwrap());
-                    Ok((conn, buf))
-                })
-            }).map(|(conn, buf)| {
-                println!("{}", std::str::from_utf8(&buf).unwrap())
-            }).boxed();
-            let result = result.map_err(|err| {
+            let mut count: i32 = 0;
+            loop {
+                if count == 10 {
+                    break;
+                }
+                result = result.and_then(|(reader, prev)| {
+                    io::read_until(reader, b'\n', vec![0u8]).and_then(|(reader, buf)| {
+                        Ok((reader, prev + String::from_utf8(buf.to_vec()).unwrap().as_str()))
+                    })
+                }).boxed();
+                count = count + 1;
+            }
+            let result = result.map(move |(_, line)| {
+                writer.write(line.as_bytes());
+                println!("{}", line)
+            }).map_err(|err| {
                 println!("IO error {:?}", err)
             }).boxed();
             handle.spawn(result);
