79e7b6c7a68c4d1823ca78ff5b978800fac08501
sxend <arimitsu.k+github@gmail.com>
2017-08-29 23:45:07 +0900

    remove redundant wrap

--- src/bin/mitm_sample.rs
+++ src/bin/mitm_sample.rs
@@ -8,8 +8,7 @@ use tokio_io::{io, AsyncRead};
 use tokio_io::io::ReadHalf;
 use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::Core;
-use std::io::BufReader;
-use std::io::Write;
+use std::io::{BufReader, Write, ErrorKind as IoErrorKind, Error as IoError};
 
 type TcpReadBuffer = BufReader<ReadHalf<TcpStream>>;
 
@@ -44,16 +43,16 @@ fn main() {
     core.run(server).unwrap();
 }
 
-fn read_until_rn(reader: TcpReadBuffer) -> BoxFuture<(TcpReadBuffer, String), std::io::Error> {
+fn read_until_rn(reader: TcpReadBuffer) -> BoxFuture<(TcpReadBuffer, String), IoError> {
     io::read_until(reader, b'\r', vec![0u8]).and_then(|(reader, buf)| {
-        vec_to_string(buf.to_vec()).and_then(move |str| Ok((reader, str)))
+        vec_to_string(buf.to_vec()).map(move |line0| (reader, line0))
     }).and_then(|(reader, line0)| {
         io::read_until(reader, b'\n', vec![0u8]).and_then(move |(reader, buf)| {
-            vec_to_string(buf.to_vec()).and_then(move |str| Ok((reader, line0 + str.as_str())))
+            vec_to_string(buf.to_vec()).map(move |line1| (reader, line0 + line1.as_str()))
         })
     }).boxed()
 }
 
-fn vec_to_string(v: Vec<u8>) -> Result<String, std::io::Error> {
-    String::from_utf8(v).map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))
+fn vec_to_string(v: Vec<u8>) -> Result<String, IoError> {
+    String::from_utf8(v).map_err(|err| IoError::new(IoErrorKind::Other, err))
 }
