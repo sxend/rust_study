7c6040db1e249c0de525efca8a8e3fead645af80
sxend <arimitsu.k+github@gmail.com>
2017-08-30 00:03:01 +0900

    reafactor future compose

--- src/bin/mitm_sample.rs
+++ src/bin/mitm_sample.rs
@@ -21,35 +21,26 @@ fn main() {
         .for_each(move |(tcp, _)| {
             let (reader, mut writer) = tcp.split();
             let reader: TcpReadBuffer = BufReader::new(reader);
-            let mut result = read_until_rn(reader);
-            let mut count: i32 = 0;
-            loop {
-                if count == 10 {
-                    break;
-                }
-                result = result.and_then(|(reader, line)| {
-                    read_until_rn(reader).and_then(move |(reader, line1)| {
-                        Ok((reader, line + line1.as_str()))
-                    })
-                }).boxed();
-                count = count + 1;
-            }
-            let result = result.and_then(move |(_, line)| {
-                writer.write(line.as_bytes()).map(move |_| println!("{}", line))
-            }).map_err(|err| println!("IO error {:?}", err)).boxed();
+            let result = read_line_until_rn(reader)
+                .and_then(move |(_, line)| {
+                    writer.write(line.as_bytes()).map(move |_| println!("{}", line))
+                })
+                .map_err(|err| println!("IO error {:?}", err)).boxed();
             handle.spawn(result);
             Ok(())
         });
     core.run(server).unwrap();
 }
 
-fn read_until_rn(reader: TcpReadBuffer) -> BoxFuture<(TcpReadBuffer, String), IoError> {
-    io::read_until(reader, b'\r', vec![0u8]).and_then(|(reader, buf)| {
-        vec_to_string(buf.to_vec()).map(move |line0| (reader, line0))
-    }).and_then(|(reader, line0)| {
-        io::read_until(reader, b'\n', vec![0u8]).and_then(move |(reader, buf)| {
-            vec_to_string(buf.to_vec()).map(move |line1| (reader, line0 + line1.as_str()))
-        })
+fn read_line_until_rn(reader: TcpReadBuffer) -> BoxFuture<(TcpReadBuffer, String), IoError> {
+    read_until_with_string(reader, b'\r').and_then(|(reader, line0)| {
+        read_until_with_string(reader, b'\n').map(move |(reader, line1)| (reader, line0 + line1.as_str()))
+    }).boxed()
+}
+
+fn read_until_with_string(reader: TcpReadBuffer, byte: u8) -> BoxFuture<(TcpReadBuffer, String), IoError> {
+    io::read_until(reader, byte, vec![0u8]).and_then(|(reader, buf)| {
+        vec_to_string(buf.to_vec()).map(move |line| (reader, line))
     }).boxed()
 }
 
